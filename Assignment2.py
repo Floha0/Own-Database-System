824976a3f18df2ffc8d4ad527f3e77460ad7a5fd
Furkan Gülhan <fgulhan07@gmail.com>
2024-12-05 14:21:14 +0300

    select, update, delete

--- Assignment2.py
+++ Assignment2.py
@@ -37,7 +37,7 @@ class main:
         table["rows"].append(values)
         print("Row inserted successfully")
 
-    def select(self, table_name, _columns):
+    def select(self, table_name, _columns, conditions):
         if table_name not in self.tables:
             print("Specified Table does not exist")
             return
@@ -46,29 +46,120 @@ class main:
         columns = table["columns"]
         rows = table["rows"]
 
-        selected_values = []
+        for i in conditions:
+            # Find the index of the column
+            index = -1
+            for column in columns:
+                index += 1
+                if column == i:
+                    break
+            
+            for row in rows:
+                if str(row[index]) == str(conditions[i]):
+                    print("Selected: ", row)
 
-        if _columns[0] == "*":
-            for i in range(len(columns)):
-                    selected_values.append(rows[i])
-        else:
-            for i in range(len(columns)):
-                if columns[i] in _columns:
-                    selected_values.append(rows[i])
+    def update(self, table_name, updates, conditions):
+        if table_name not in self.tables:
+            print("Specified Table does not exist")
+            return
+        
+        table = self.tables[table_name]
+        columns = table["columns"]
+        rows = table["rows"]
+
+        for i in conditions:
+            # Find the index of the column
+            index = -1
+            for column in columns:
+                index += 1
+                if column == i:
+                    break
+
+            
+            for row in rows:
+                if row[index] == conditions[i]:
+                    # Update the row
+                    for update in updates:
+                        index = -1
+                        for column in columns:
+                            index += 1
+                            if column == update:
+                                break
+                        row[index] = updates[update]
+    
+    def delete(self, table_name, conditions):
+        if table_name not in self.tables:
+            print("Specified Table does not exist")
+            return
+        
+        table = self.tables[table_name]
+        columns = table["columns"]
+        rows = table["rows"]
 
 
-        print("Selected Values: ", selected_values)
+        can_delete = True
+        rows_to_delete = []
+
+        index = -1
+        for i in conditions:
+            index = -1
+            for column in columns:
+                index += 1
+                if column == i:
+                    break
+
+            found = False
+            for row in rows:
+                if row[index] == conditions[i]:
+                    # rows.remove(row)
+                    rows_to_delete.append(row)
+                    found = True
+                    break
+            if not found:
+                can_delete = False
+                break
+
+        if can_delete:
+            for row in rows_to_delete:
+                if row in rows:
+                    rows.remove(row)
+            print("Rows deleted successfully")
+    
+    def join(self, table1, table2, _column):
+        if table1 not in self.tables or table2 not in self.tables:
+            print("Specified Table does not exist")
+            return
+        
+
+sample_cmd = 'SELECT students id,name WHERE {"major": "CS"}'
+
+parts = sample_cmd.strip().split(" ", 2)
+cmd = parts[0].upper()      # SELECT, INSERT etc.
+table_name, update_str = parts[1], parts[2]
+# update_str is the rest of the text without cmd and table name
+updates_part, where_part = update_str.split(" WHERE ")
+
+# Parse updates and conditions
+updates = updates_part.split(",")
+conditions = eval(where_part)
+
+print("Updates: ", updates)
+print("conditions: ", conditions)
+
 
 
 m = main()
-m.create_table("students", ["id", "name", "age"])
-m.insert("students", [1,"a",17])
+# m.create_table("students", ["id", "name", "age"])
+# m.insert("students", ["1","a","17"])
 
 m.create_table("courses", ["course_id","name","major"])
-m.insert("courses", [101,"Intro to Programming","CS"])
-m.insert("courses", [102,"Circuit Design","EE"])
+m.insert("courses", ["101","Intro to Programming","CS"])
+m.insert("courses", ["102","Circuit Design","EE"])
+m.insert("courses", ["103","Fundamentals of Physics","PE"])
 
-m.select("courses", ["course_id"])
+m.select("courses", ["course_id"], {"major": "EE", "course_id":"101"})
+m.update("courses", {"course_id": "51", "major": "IE"}, {"major": "CS"})
+m.delete("courses", {"major": "PE", "course_id":"103"})
 
 print(m.tables)
 
